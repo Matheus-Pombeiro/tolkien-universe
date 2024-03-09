b630f75fd4f521cbff4a638c3affdc4ec0ca2216
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-03-09 16:24:34 -0300

    ADD: the books json object reference

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import { Component } from 'react';
+
 import './App.css';
 
 import Header from './components/Header';
+import Main from './components/Main';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+  state = {
+    books: []
+  }
 
-      
-    </div>
-  );
+  componentDidMount() {
+    fetch("/objectsJson/books.json")
+    .then(response => response.json())
+    .then(books => this.setState({ books }))
+    .catch(function(error) {
+      console.log("Request error");
+    })
+    .finally(function() {
+      console.log("Always return");
+    });
+  };
+
+  render() {
+    return (
+      <div className="App h-dvh w-dvw bg-stone-100 dark:bg-stone-400">
+        <Header />
+        <Main 
+          books = { this.state.books }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
