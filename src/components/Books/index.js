f2d32901769f37d29ad50d1100784c315c661a22
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-03-10 14:38:56 -0300

    ADD: style components to Books

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,17 +1,18 @@
 import { useTranslation } from "react-i18next";     // Translation framework import
 
-const Books = (props) => {
+const Books = (props) => {     // Books component 
 
-    const { t } = useTranslation();
+    const { t } = useTranslation();     // Translation method
 
+    // JSX
     return (
-        <section className="w-5/6 xl:w-10/12 mx-auto p-2 xl:p-4 bg-stone-300  flex flex-col justify-evenly items-center gap-5 rounded-md drop-shadow-md">
-            <h2 className="text-lg font-medium">{t("Books")}</h2>
+        <section className="section">
+            <h2 className="h2">{t("Books")}</h2>
             <p>{t("Books Description")}</p>
-            <div className="max-w-full flex justify-center items-center flex-wrap gap-5">
-                {props.books.map((book, index) => (
-                    <div key={book.id} className="w-80 h-32 bg-stone-200 flex flex-col justify-center items-center gap-2 rounded border border-stone-400">
-                        <h3 className="font-medium">{book.name}</h3>
+            <div className="item-container">
+                {props.books.map((book) => (    /* books json object mapping */
+                    <div key={book.id} className="item">
+                        <h3 className="h3">{book.name}</h3>
                         <p>{t("Author")}: {book.author}</p>
                         <p>{t("Publishing Year")}: {book.publishingYear}</p>
                     </div>
